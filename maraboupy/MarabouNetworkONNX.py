626591ecc4b265bb472318ce09e2c0f9044daeb4
datvo06 <datvo06@gmail.com>
2022-04-05 23:28:33 +0700

    Minor optimization on concat

--- maraboupy/MarabouNetworkONNX.py
+++ maraboupy/MarabouNetworkONNX.py
@@ -22,6 +22,57 @@ from maraboupy import MarabouNetwork
 from onnx import TensorProto
 import itertools
 
+
+
+# The below ScatterElements' numpy implementation is from https://stackoverflow.com/a/46204790/11767360
+def scatter_elements(data, indices, updates, axis=0, reduction='none'):  # type: ignore
+    if axis < 0:
+        axis = data.ndim + axis
+
+    idx_xsection_shape = indices.shape[:axis] + indices.shape[axis + 1:]
+
+    def make_slice(arr, axis, i):  # type: ignore
+        slc = [slice(None)] * arr.ndim
+        slc[axis] = i
+        return slc
+
+    def unpack(packed):  # type: ignore
+        unpacked = packed[0]
+        for i in range(1, len(packed)):
+            unpacked = unpacked, packed[i]
+        return unpacked
+
+    def make_indices_for_duplicate(idx):  # type: ignore
+        final_idx = list()
+        for i in range(len(idx[0])):
+            final_idx.append(tuple(idx_element[i] for idx_element in idx))
+        return list(final_idx)
+
+    # We use indices and axis parameters to create idx
+    # idx is in a form that can be used as a NumPy advanced indices for scattering of updates param. in data
+    idx = [[unpack(np.indices(idx_xsection_shape).reshape(indices.ndim - 1, -1)),
+            indices[tuple(make_slice(indices, axis, i))].reshape(1, -1)[0]] for i in range(indices.shape[axis])]
+    idx = list(np.concatenate(idx, axis=1))
+    idx.insert(axis, idx.pop())
+
+    # updates_idx is a NumPy advanced indices for indexing of elements in the updates
+    updates_idx = list(idx)
+    updates_idx.pop(axis)
+    updates_idx.insert(axis, np.repeat(np.arange(indices.shape[axis]), np.prod(idx_xsection_shape)))
+
+    scattered = np.copy(data)
+    if reduction == 'none':
+        scattered[tuple(idx)] = updates[tuple(updates_idx)]
+    else:
+        idx, updates_idx = make_indices_for_duplicate(idx), make_indices_for_duplicate(updates_idx)
+        for iter, idx_set in enumerate(idx):
+            if reduction == 'add':
+                scattered[idx_set] += updates[updates_idx[iter]]
+            elif reduction == 'mul':
+                scattered[idx_set] *= updates[updates_idx[iter]]
+    return scattered
+
+
 class MarabouNetworkONNX(MarabouNetwork.MarabouNetwork):
     """Constructs a MarabouNetworkONNX object from an ONNX file
 
@@ -486,6 +537,9 @@ class MarabouNetworkONNX(MarabouNetwork.MarabouNetwork):
                     e.setScalar(const_val[i])
                     self.addEquation(e)
                 var_arrays.append(var_val)
+        for i in range(len(var_arrays)):
+            var_arrays[i] = np.broadcast_to(var_arrays[i], var_arrays[0].shape)
+        print(list(v.shape for v in var_arrays))
         self.varMap[node.output[0]] = np.concatenate(
                 tuple(var_arrays),
                 axis=axis)
@@ -678,6 +732,7 @@ class MarabouNetworkONNX(MarabouNetwork.MarabouNetwork):
         if len(axis) > 1:
             raise NotImplementedError("ReduceMax yet accept multidim max")
         axis = axis[0]
+        print(axis)
         inputName = node.input[0]
         if inputName in self.constantMap:
             self.constantMap[nodeName] = np.amax(
@@ -686,9 +741,11 @@ class MarabouNetworkONNX(MarabouNetwork.MarabouNetwork):
         else:
             # Extract attributes and define shape
             inputShape = self.shapeMap[node.input[0]]
+            print(inputShape)
             if axis < 0:
-                axis = len(inputShape) - 1
+                axis = len(inputShape) + axis
             outputShape = [e for i, e in enumerate(list(inputShape)) if i != axis]
+            print(outputShape)
             self.shapeMap[nodeName] = outputShape
             outputVar = self.makeNewVariables(nodeName)
             ranges = list([list(range(e)) for e in outputShape])
@@ -954,6 +1011,9 @@ class MarabouNetworkONNX(MarabouNetwork.MarabouNetwork):
             return
 
         multiple = self.constantMap[inputName2]
+        if inputName1 in self.constantMap:
+            self.constantMap[nodeName] = self.constantMap[inputName1] * self.constantMap[inputName2]
+            return
         input1 = self.varMap[inputName1]
         outputVariables = self.makeNewVariables(nodeName)
         input1 = input1.reshape(-1)
