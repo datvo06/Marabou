e86c56726021499d4f08b6bfddacdb65082746e0
datvo06 <datvo06@gmail.com>
2022-04-10 15:08:10 +0700

    minor

--- maraboupy/MarabouNetworkONNX.py
+++ maraboupy/MarabouNetworkONNX.py
@@ -133,6 +133,45 @@ def scatter_elements_var(data, indices, updates, axis=0, reduction='none'):  # t
     return scattered
 
 
+def gather_nd_impl(data: np.ndarray, indices: np.ndarray, batch_dims: int) -> np.ndarray:
+    # Note the data rank - will be reused multiple times later
+    data_rank = len(data.shape)
+
+    # Check input tensors' shape/rank condition
+    assert indices.shape[-1] <= data_rank
+
+    #The list of data/indice shape of batch_dims
+    batch_dims_shape = []
+
+    #The number of elements in the batch_dims for data/indice array
+    batch_dims_size = 1
+
+    # Check the shape of indice and data are identicial for batch dims.
+    for i in range(batch_dims):
+        batch_dims_shape.append(indices.shape[i])
+        batch_dims_size *= indices.shape[i]
+
+    # Compute output of the op as below
+
+    # Compute shape of output array
+    output_shape = batch_dims_shape + list(indices.shape)[batch_dims:-1] if (indices.shape[-1] == data_rank - batch_dims) \
+     else batch_dims_shape + list(indices.shape)[batch_dims:-1] + list(data.shape)[batch_dims + indices.shape[-1]:]
+
+    # Placeholder for output data
+    output_data_buffer = []
+
+    # Flatten 'indices' to 2D array
+    reshaped_indices = indices.reshape(batch_dims_size, -1, indices.shape[-1])
+
+    # Flatten 'data' to array of shape (batch_dim_size, data.shape[batch_dimes:])
+    reshaped_data = data.reshape((batch_dims_size, ) + data.shape[batch_dims:])
+
+    # gather each scalar value from 'data'
+    for batch_dim in range(reshaped_indices.shape[0]):
+        for outer_dim in range(reshaped_indices.shape[1]):
+            gather_index = tuple(reshaped_indices[batch_dim][outer_dim])
+            output_data_buffer.append(reshaped_data[(batch_dim,) + gather_index])
+    return np.asarray(output_data_buffer, dtype=data.dtype).reshape(output_shape)
 
 class MarabouNetworkONNX(MarabouNetwork.MarabouNetwork):
     """Constructs a MarabouNetworkONNX object from an ONNX file
@@ -553,12 +592,12 @@ class MarabouNetworkONNX(MarabouNetwork.MarabouNetwork):
         if inputName2 not in self.constantMap:
             raise NotImplementedError("GatherND of arbitrary size not allowed")
         if inputName1 in self.constantMap:
-            self.constantMap[node.output[0]] = np.take(
+            self.constantMap[node.output[0]] = gather_nd_impl(
                 self.constantMap[inputName1],
                 self.constantMap[inputName2])
             self.shapeMap[node.output[0]] = self.constantMap[node.output[0]]
         else:
-            self.varMap[node.output[0]] = np.take(self.varMap[inputName1],
+            self.varMap[node.output[0]] = gather_nd_impl(self.varMap[inputName1],
                                                   self.constantMap[inputName2],
                                                   )
             self.shapeMap[node.output[0]] = self.varMap[node.output[0]].shape
