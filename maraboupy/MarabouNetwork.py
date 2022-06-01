98c55a944d637f5b4e29fa377e5966d1b612a87f
datvo06 <datvo06@gmail.com>
2022-06-01 15:11:37 +0700

    minor

--- maraboupy/MarabouNetwork.py
+++ maraboupy/MarabouNetwork.py
@@ -145,7 +145,21 @@ class MarabouNetwork:
         Args:
             disjuncts (list of list of Equations): Each inner list represents a disjunct
         """
-        self.disjunctionList.append(disjuncts)
+        converted_disjuncts = []
+        for dlist in disjuncts:
+            converted_disjuncts.append([])
+            for oeq in dlist:
+                if isinstance(oeq, MarabouUtils.Equation):
+                    eq = MarabouCore.Equation(oeq.EquationType)
+                    for (c, v) in oeq.addendList:
+                        assert v < self.numVars
+                        eq.addAddend(c, v)
+                    eq.setScalar(oeq.scalar)
+                else:
+                    eq = oeq
+                converted_disjuncts[-1].append(eq)
+
+        self.disjunctionList.append(converted_disjuncts)
 
     def lowerBoundExists(self, x):
         """Function to check whether lower bound for a variable is known
