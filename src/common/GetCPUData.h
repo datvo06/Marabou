ce187f0e208fd62b9588064a6195869229d3f6a0
datvo06 <datvo06@gmail.com>
2024-12-03 18:20:57 +0700

    Minor fixes

--- src/common/GetCPUData.h
+++ src/common/GetCPUData.h
@@ -16,27 +16,55 @@
 #ifndef __GetCPUData_h__
 #define __GetCPUData_h__
 
-#if defined( __linux__ )
+#if defined(__linux__)
 #include <sched.h>
-#define getCPUId( cpu ) { cpu = sched_getcpu(); }
+#define getCPUId(cpu)                                                          \
+  {                                                                            \
+    cpu = sched_getcpu();                                                      \
+  }
 #elif __APPLE__
+#if (!defined(__arm64__))
 #include <cpuid.h>
 
-#define CPUId( INFO, LEAF, SUBLEAF ) __cpuid_count( LEAF, SUBLEAF, INFO[0],  \
-                                                    INFO[1], INFO[2], INFO[3] )
-#define getCPUId( cpu ) {                           \
-    uint32_t CPUInfo[4];                            \
-    CPUId( CPUInfo, 1, 0 );                         \
-    if ( ( CPUInfo[3] & ( 1 << 9 ) ) == 0) {        \
-      cpu = -1;                                     \
-    }                                               \
-    else {                                          \
-      cpu = ( unsigned )CPUInfo[1] >> 24;           \
-    }                                               \
-    if ( cpu < 0 ) cpu = 0;                         \
+#define CPUId(INFO, LEAF, SUBLEAF)                                             \
+  __cpuid_count(LEAF, SUBLEAF, INFO[0], INFO[1], INFO[2], INFO[3])
+#define getCPUId(cpu)                                                          \
+  {                                                                            \
+    uint32_t CPUInfo[4];                                                       \
+    CPUId(CPUInfo, 1, 0);                                                      \
+    if ((CPUInfo[3] & (1 << 9)) == 0) {                                        \
+      cpu = -1;                                                                \
+    } else {                                                                   \
+      cpu = (unsigned)CPUInfo[1] >> 24;                                        \
+    }                                                                          \
+    if (cpu < 0)                                                               \
+      cpu = 0;                                                                 \
   }
 #else
-#define getCPUId( cpu ) "#"
+#include <string.h>
+#include <sys/sysctl.h>
+#include <sys/types.h>
+
+#define getCPUId(cpu)                                                          \
+  {                                                                            \
+    int hasNeon = 0;                                                           \
+    size_t size = sizeof(hasNeon);                                             \
+    if (sysctlbyname("hw.optional.neon", &hasNeon, &size, NULL, 0) == 0) {     \
+      if (hasNeon) {                                                           \
+        cpu = 1;                                                               \
+      } else {                                                                 \
+        cpu = -1;                                                              \
+      }                                                                        \
+    } else {                                                                   \
+      cpu = -1;                                                                \
+    }                                                                          \
+                                                                               \
+    if (cpu < 0)                                                               \
+      cpu = 0;                                                                 \
+  }
+#endif
+#else
+#define getCPUId(cpu) "#"
 #endif
 
 #endif // __GetCPUData_h__
